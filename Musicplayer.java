ccf4a42cc126f64ce4d25c5ba821b21e1142546f
biruk2k <biruktikue19@gmail.com>
2022-05-19 15:40:40 -0700

    first commit

---
+++ Musicplayer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package musicplayer;
+
+/**
+ *
+ * @author BIRUK 2K
+ */
+
+public class Musicplayer {
+    public static void main(String[] args){
+        new mainframe().setVisible(true);
+    }
+}
